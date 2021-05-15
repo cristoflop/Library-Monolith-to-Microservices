ddb7ec99758d39a27241c05e9df19660bafa56f0
cristoflop <cristofer.lc.99@gmail.com>
2021-05-15 15:32:17 +0200

    Added users controller

---
+++ users/src/main/java/es/codeurjc/users/controllers/UserController.java
@@ -0,0 +1,51 @@
+package es.codeurjc.users.controllers;
+
+import es.codeurjc.users.dtos.requests.UpdateUserEmailRequestDto;
+import es.codeurjc.users.dtos.requests.UserRequestDto;
+import es.codeurjc.users.dtos.responses.UserResponseDto;
+import es.codeurjc.users.services.UserService;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public Collection<UserResponseDto> getUsers() {
+        return this.userService.findAll();
+    }
+
+    @GetMapping("/{userId}")
+    public UserResponseDto getUser(@Parameter(description = "id of user to be searched")
+                                   @PathVariable long userId) {
+        return this.userService.findById(userId);
+    }
+
+    @PostMapping("/")
+    public UserResponseDto createUser(@Valid @RequestBody UserRequestDto userRequestDto) {
+        return this.userService.save(userRequestDto);
+    }
+
+    @PatchMapping("/{userId}")
+    public UserResponseDto updateUserEmail(@Parameter(description = "id of user to update the email")
+                                           @PathVariable long userId,
+                                           @Valid @RequestBody UpdateUserEmailRequestDto updateUserEmailRequestDto) {
+        return this.userService.updateEmail(userId, updateUserEmailRequestDto);
+    }
+
+    @DeleteMapping("/{userId}")
+    public UserResponseDto deleteUser(@Parameter(description = "id of user to be deleted") @PathVariable long userId) {
+        return this.userService.delete(userId);
+    }
+
+}
