b07b87254c28a830055ed6453fdc998d6a0efda7
cristoflop <cristofer.lc.99@gmail.com>
2021-05-15 15:30:38 +0200

    Added dtos

---
+++ users/src/main/java/es/codeurjc/users/dtos/requests/UserRequestDto.java
@@ -0,0 +1,18 @@
+package es.codeurjc.users.dtos.requests;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class UserRequestDto {
+
+    @NotBlank(message = "Nick is mandatory")
+    private String nick;
+
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Invalid email")
+    private String email;
+
+}
